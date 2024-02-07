230de1e04c421faf85bd00c3d953075877776243
elaman sargaldaev <melisbekovelaman311@gmail.com>
2024-02-07 16:16:27 +0600

    написал логику на get запрос

--- api/routers/links.ts
+++ api/routers/links.ts
@@ -6,26 +6,44 @@ const linksRouters = express.Router();
 
 
 linksRouters.post('/', async (req, res) => {
+  try {
+    const alphabet = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+    let result = '';
 
-  const alphabet = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-  let result = '';
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * alphabet.length);
+      result += alphabet[randomIndex];
+    }
 
-  for (let i = 0; i < 6; i++) {
-    const randomIndex = Math.floor(Math.random() * alphabet.length);
-    result += alphabet[randomIndex];
+    const linkCreate: ILinkCreate = {
+      url: req.body.url,
+      shortUrl: result
+    };
+
+    const savedLink = new Link(linkCreate);
+    await savedLink.save();
+
+    res.send(savedLink);
+  } catch (e) {
+    res.status(500).send(e);
   }
-
-
-  const linkCreate: ILinkCreate = {
-    url: req.body.url,
-    shortUrl: result
-  };
-
-
-  const savedLink = new Link(linkCreate);
-  await savedLink.save();
-
-  res.send(savedLink);
 });
+
+
+linksRouters.get('/:shortUrl', async (req, res) => {
+  try {
+    const shortUrl = req.params.shortUrl;
+
+    const link = await Link.findOne({shortUrl});
+    if (link) {
+      return res.status(301).redirect(link.url);
+    }
+
+    res.sendStatus(404);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 
 export default linksRouters;
